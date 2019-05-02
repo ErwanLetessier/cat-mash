498325027807729e7dcca357ed676acf99d60655
Erwan Letessier <erwan.letessier@gmail.com>
2019-05-03 00:24:26 +0200

    serve index.html through a REST controller cannot find out the clean solution: static resources must definitly be served directly

---
+++ src/main/java/com/exercise/catmash/controller/WebController.java
@@ -0,0 +1,37 @@
+package com.exercise.catmash.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+
+@RestController
+public class WebController {
+
+  private String index = loadFromClasspathFile("static/index.html");
+
+  private String loadFromClasspathFile(String filename) {
+    try {
+      InputStream inputStream = getClass().getClassLoader().getResourceAsStream(filename);
+      ByteArrayOutputStream result = new ByteArrayOutputStream();
+      byte[] buffer = new byte[1024];
+      int length;
+      while ((length = inputStream.read(buffer)) != -1) {
+        result.write(buffer, 0, length);
+      }
+      return result.toString(StandardCharsets.UTF_8.name());
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+  public String index() {
+    return index;
+  }
+
+}
